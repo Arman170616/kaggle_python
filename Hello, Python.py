1ea19d3b8133138852eebbf918ecc66cf9acf275
Arman170616 <armanice09@gmail.com>
2022-04-09 20:12:03 +0600

    refresh your mind

--- Hello, Python.py
+++ Hello, Python.py
@@ -1,1 +1,16 @@
-print("arman")
+#Python was named for the British comedy troupe Monty Python, so we'll make our first Python program a homage to their skit about Spam).
+
+spam_amount = 0
+#print(spam_amount)
+
+spam_amount = spam_amount +4
+
+print(spam_amount)
+
+
+
+if spam_amount > 0:
+    print("But I don't want ANY spam!")
+
+viking_song = "Spam " * spam_amount
+print(viking_song)
