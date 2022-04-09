82a06010a6b57246da0685bcf02f87187f570dbb
Arman170616 <armanice09@gmail.com>
2022-04-10 04:36:15 +0600

    order of operations example 1

---
+++ orderOfOperations.py
@@ -0,0 +1,13 @@
+hat_height_cm = 25
+my_height_cm = 190
+
+
+total_height_cm = hat_height_cm + my_height_cm / 100
+
+print("Height in Meters= ", total_height_cm, "?")
+
+
+# Height in Meters=  26.9 ?
+
+#Parentheses are useful here. You can add them to force Python to evaluate sub-expressions in whatever order you want.
+
